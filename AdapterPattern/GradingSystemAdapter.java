c6b2722ed3f32e0b655ea7ee567154e616f098cd
Jasmine Mae Ferrer <ferrerjasminemae04@gmail.com>
2026-09-21 15:16:32 +0800

    Create GradingSystemAdapter.java

---
+++ AdapterPattern/GradingSystemAdapter.java
@@ -0,0 +1,13 @@
+class GradingSystemAdapter implements SchoolManagementApp {
+    private GradingSystem gradingSystem;
+
+    public GradingSystemAdapter(GradingSystem gradingSystem) {
+        this.gradingSystem = gradingSystem;
+    }
+
+    @Override
+    public void integrateSystem() {
+        gradingSystem.recordGrades();
+    }
+}
+
