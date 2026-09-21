294269c965c5f17c93bc095161047e792e02fa7b
Jasmine Mae Ferrer <ferrerjasminemae04@gmail.com>
2026-09-21 15:17:36 +0800

    Create Main.java

---
+++ AdapterPattern/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        AttendanceSystem attendance = new AttendanceSystem();
+        GradingSystem grading = new GradingSystem();
+        LibrarySystem library = new LibrarySystem();
+
+        SchoolManagementApp attendanceAdapter =
+                new AttendanceSystemAdapter(attendance);
+
+        SchoolManagementApp gradingAdapter =
+                new GradingSystemAdapter(grading);
+
+        SchoolManagementApp libraryAdapter =
+                new LibrarySystemAdapter(library);
+
+        attendanceAdapter.integrateSystem();
+        gradingAdapter.integrateSystem();
+        libraryAdapter.integrateSystem();
+    }
+}
